f5884ed6bdfab21a6d5ecba1090cf55feb345ff7
zidanabbas <zidan.abbas28@gmail.com>
2024-04-26 00:14:11 +0700

    create a ui

--- src/components/fragments/Navbar.vue
+++ src/components/fragments/Navbar.vue
@@ -5,6 +5,9 @@ import { RouterLink } from 'vue-router'
 <template>
   <header class="header">
     <div class="brand">
+      <div class="logo">
+        <img src="@/assets/djp-pajak.jpg" width="100px" alt="logo" />
+      </div>
       <RouterLink to="/">BayarPajak</RouterLink>
     </div>
     <div class="time">
@@ -36,31 +39,40 @@ export default {
   justify-content: space-between;
   height: 60px;
   padding: 0 40px;
-  background-color: #faae2b;
+  background-color: #fcec27;
 }
 
 .brand {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   font-weight: bold;
   font-size: large;
-  color: white;
-  /* border: 1px solid white; */
-  padding: 5px;
+  padding: 0 5px;
+  gap: 10px;
+}
+
+.brand .logo {
+  display: flex;
+  width: 100px;
+  justify-content: center;
+  align-items: center;
 }
 
 .brand a {
   text-decoration: none;
-  color: white;
+  color: #343a40;
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
 
 .brand a:hover {
-  color: black;
+  color: rgb(72, 72, 72);
 }
 
 .time {
   display: flex;
   padding: 2px;
-  color: whitesmoke;
+  color: #343a40;
   font-size: 1rem;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,50 @@
 <template>
-  <main class="container">
-    <div class="container-list-antrian">
-      <div class="list-antrian">
-        <h1 class="title-list">List Antrian</h1>
-        <div class="number-antrian">
-          <span>1</span>
+  <main id="home">
+    <div class="container">
+      <!-- container-banner -->
+      <div class="container-banner">
+        <div class="list-antrian">
+          <h1 class="title-list">List Antrian</h1>
+          <div class="number-antrian">
+            <span>1</span>
+          </div>
+        </div>
+        <div class="frame">
+          <iframe
+            width="100%"
+            height="100%"
+            :src="youtubeUrl"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
+      <!-- end container-banner -->
     </div>
-    <div class="frame">
-      <iframe
-        width="100%"
-        height="100%"
-        :src="youtubeUrl"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
+    <div class="container">
+      <div class="grid-container">
+        <div class="card">
+          <h1 class="title-list">Loket 1</h1>
+          <div class="card-body">
+            <div class="number-antrian">20</div>
+          </div>
+        </div>
+        <div class="card">
+          <h1 class="title-list">Loket 2</h1>
+          <div class="card-body">
+            <div class="number-antrian">20</div>
+          </div>
+        </div>
+        <div class="card">
+          <h1 class="title-list">Loket 3</h1>
+          <div class="card-body">
+            <div class="number-antrian">20</div>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -37,27 +63,31 @@ export default {
 .container {
   display: flex;
   width: 100%;
-  padding: 5px 40px;
-  background-color: #232946;
-  gap: 20px;
+  padding: 20px 40px;
+  /* background-color: #6f42c1; */
+  /* border: 1px solid white; */
 }
 
-.container-list-antrian {
-  width: 40%;
-  height: 400px;
+.container-banner {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  gap: 20px;
+  padding: 0 20px;
+  /* background-color: aqua; */
   /* border: 1px solid black; */
 }
 
 .list-antrian {
   display: flex-wrap;
   justify-content: center;
-  gap: 20px;
-  width: 100%;
-  height: 100%;
-  background-color: #ffd803;
+  width: 500px;
+  height: 325px;
+  background-color: #6f42c1;
+  border-radius: 20px;
 }
 
-.list-antrian .title-list {
+.title-list {
   text-align: center;
   font-size: 20px;
   font-weight: bold;
@@ -85,7 +115,41 @@ export default {
 
 .frame {
   width: 60%;
-  height: 400px;
-  /* background-color: green; */
+  height: 325px;
+}
+
+.card {
+  justify-self: center;
+  width: 350px;
+  height: 200px;
+  background-color: #6f42c1;
+  color: white;
+  padding: 10px;
+  border-radius: 20px;
+  overflow: hidden;
+}
+
+.card-body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80%;
+  /* border: 1px solid black; */
+}
+
+.card-body .number-antrian {
+  font-size: 50px;
+  font-weight: bold;
+}
+
+.grid-container {
+  display: grid;
+  width: 100%;
+  padding: 5px;
+  grid-auto-flow: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
 }
 </style>
